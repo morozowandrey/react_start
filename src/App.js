a2196b0071fe33e09f033f414ad58c4b5eb9764f
Andrew Morozow <andrey940@gmail.com>
2017-12-06 17:13:31 +0200

    structure setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+// class App extends React.Component {
+//     render(){
+//         return <h1>heavy metall</h1>
+//     }
+// }
+
+const App = () => <h1>heavy metall</h1>
+
+export default App
